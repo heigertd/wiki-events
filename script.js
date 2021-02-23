588a51f5036f68f63b4bd36ab82f2cd0ec0be2ff
Devin Heigert <heigertd@gmail.com>
2021-02-22 16:51:58 -0800

    heat map and today calls working

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+//Current Day events
+function currentDay() {
+    var month = new Date().getMonth();
+    var day = new Date().getDate();
+
+    var queryURL = "https://byabbe.se/on-this-day/" + month + "/" + day + "/events.json";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (res) {
+        $("#date0").text(res.date);
+        $("#year0").text(res.events[0].year);
+        $("#description0").text(res.events[0].description);
+    })
+
+}
+
 //Get user input and make API Request
 $("#submit").on("click", function (event) {
     event.preventDefault();
@@ -15,68 +32,85 @@ $("#submit").on("click", function (event) {
     })
 })
 
-
+//Create row for search result
 var createRow = function (data) {
-    // Create a new table row element
-    var results = $("#search-results");
 
-    // Methods run on jQuery selectors return the selector they we run on
-    // This is why we can create and save a reference to a td in the same statement we update its text
     $("#date").text(data.date);
     $("#year").text(data.events[0].year);
     $("#description").text(data.events[0].description);
-    // var actorsp = $("<td>").text(data.Actors);
-
-    // Append the newly created table data to the table row
-    // results.prepend(datep, yearp, eventp);
-    // Append the table row to the table body
-    // results.append(tRow);
 };
 
-function everyDay() {
+//Functions for getting all the dates and sorting them
+async function sortDates() {
+
+    let allDates = await everyDay();
+
+    useData(allDates);
+}
+
+async function everyDay() {
+    var dates = [];
     for (let i = 1; i < 13; i++) {
         if (i === 1 || i === 3 || i === 5 || i === 7 || i === 8 || i === 10 || i === 12) {
             for (let j = 1; j < 32; j++) {
                 var queryURL = "https://byabbe.se/on-this-day/" + i + "/" + j + "/events.json";
-                $.ajax({
+                await $.ajax({
                     url: queryURL,
                     method: "GET"
                 }).then(function (response) {
-                    // createRow(response);
-                    console.log(response)
+                    dates.push(response);
                 })
             }
 
-        // } else if (i === 4 || i === 6 || i === 9 || i === 11) {
-        //     for (let k = 1; k < 32; k++) {
-        //         for (let j = 1; j < 32; j++) {
-        //             var queryURL = "https://byabbe.se/on-this-day/" + i + "/" + k + "/events.json";
-        //             $.ajax({
-        //                 url: queryURL,
-        //                 method: "GET"
-        //             }).then(function (response) {
-        //                 // createRow(response);
-        //                 console.log(response)
-        //             })
-        //         }
-        //     }
-        // } else {
-        //     for (let l = 1; l < 29; l++) {
-        //         for (let j = 1; j < 32; j++) {
-        //             var queryURL = "https://byabbe.se/on-this-day/" + i + "/" + l + "/events.json";
-        //             $.ajax({
-        //                 url: queryURL,
-        //                 method: "GET"
-        //             }).then(function (response) {
-        //                 // createRow(response);
-        //                 console.log(response)
-        //             })
-        //         }
-        //     }
-        // }
+        } else if (i === 4 || i === 6 || i === 9 || i === 11) {
+            for (let k = 1; k < 31; k++) {
+
+
+                var queryURL = "https://byabbe.se/on-this-day/" + i + "/" + k + "/events.json";
+                await $.ajax({
+                    url: queryURL,
+                    method: "GET"
+                }).then(function (response) {
+                    dates.push(response);
+                })
+            }
+
+        } else {
+            for (let l = 1; l < 30; l++) {
+                var queryURL = "https://byabbe.se/on-this-day/" + i + "/" + l + "/events.json";
+                await $.ajax({
+                    url: queryURL,
+                    method: "GET"
+                }).then(function (response) {
+
+                    dates.push(response);
+                })
+            }
+        }
     }
 
-}
+    return dates;
+
 }
 
-everyDay();
+
+//Heat map
+function useData(value) {
+
+    for (i = 0; i < value.length; i++) {
+
+        var dateDiv = $('<div class = "heat-map-day">').css('background-color', `rgb(${value[i].events.length * 3}, 0, 0)`);
+        var dateP = $('<p>').text(value[i].date)
+
+        dateDiv.append(dateP);
+
+        $('.heat-map-year').append(dateDiv);
+
+    }
+
+
+}
+
+currentDay();
+sortDates();
+
